25008f4dd7c33a5d61e6edab3a3004aa2e0ecd64
Brian Pruitt <brianrpruitt63@gmail.com>
2022-04-21 20:50:53 -0400

    set up for deploy

--- sporty-seed.sql
+++ sporty-seed.sql
@@ -1,31 +1,6 @@
 INSERT INTO users (email, pwd, first_name, last_name, super_admin)
-VALUES ('test@test.com',
+VALUES ('admin@sporty.com',
         '$2b$12$gJCw24a1UU23B1wLVufMZeBCEgYnketxQ.AgRIgRex/XNCwKqOG/y',
-        'Bob',
-        'Testy',
-        TRUE),
-        ('test2@test.com',
-        '$2b$12$gJCw24a1UU23B1wLVufMZeBCEgYnketxQ.AgRIgRex/XNCwKqOG/y',
-        'Barb',
-        'Tasty',
-        FALSE),
-        ('test3@test.com',
-        '$2b$12$gJCw24a1UU23B1wLVufMZeBCEgYnketxQ.AgRIgRex/XNCwKqOG/y',
-        'Bulb',
-        'Toasty',
-        FALSE);
-
-INSERT INTO organizations (org_name)
-VALUES  ('Org1'),
-        ('Org2');
-
-INSERT INTO user_organizations (email, org_id, admin_level)
-VALUES ('test3@test.com',
-        1,
-        3),
-        ('test2@test.com',
-        1,
-        2),
-        ('test2@test.com',
-        2,
-        1);
+        'Sporty',
+        'Admin',
+        TRUE)
